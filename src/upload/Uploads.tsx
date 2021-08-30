e9ef7dca2623c9e965ded89162d0809a5586e8fe
Chetan <chetan.pandey@isentia.com>
2021-08-30 16:54:08 +0800

    format change

--- src/upload/Uploads.tsx
+++ src/upload/Uploads.tsx
@@ -1,25 +1,25 @@
 import Item from "../model/Item";
 import Upload from "./Upload";
 
-export default function Uploads(props:any) {
+export default function Uploads(props: any) {
 
-    const content = props.items.map((item:Item) =>
-     <Upload item={item}/>
-  );
+    const content = props.items.map((item: Item) =>
+        <Upload item={item} />
+    );
     return (
         <div>
-        Uploads 
-        <table>
-            <tr>
-                <th>Index</th>
-                <th>Date</th>
-                <th>Status</th>
-                <th>Rerun</th>
-            </tr>
-            <tbody>
-                {content}
-            </tbody>     
-        </table>
+            Uploads
+            <table>
+                <tr>
+                    <th>Index</th>
+                    <th>Date</th>
+                    <th>Status</th>
+                    <th>Rerun</th>
+                </tr>
+                <tbody>
+                    {content}
+                </tbody>
+            </table>
         </div>
     );
-  }
+}
