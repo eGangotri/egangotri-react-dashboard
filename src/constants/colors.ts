be113447fedcbb1c4e96f7773a5f53d35c83b0c2
eGangotri Digital Preservation Trust <97491215+chetanbiqmind@users.noreply.github.com>
2022-11-06 00:55:59 +0800

    date filter

---
+++ src/constants/colors.ts
@@ -0,0 +1,37 @@
+/* ***************************************************************************
+ * Copyright © Biqmind Pte Ltd – 2021 - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * ************************************************************************ */
+export const BLACK = "#000000";
+export const WHITE = "#FFFFFF";
+export const PRIMARY_BLUE = "#34A3D3";
+export const BLACK_PEARL = "#1F2325";
+export const AMERICAN_SILVER = "#CFCFD2";
+export const FADED_WHITE = "#F9F9F9";
+export const GRAY34 = "#575757";
+export const WHITE_SMOKE = "#F8F8F8";
+export const SUCCESS_GREEN = "#5BAB62";
+export const FADED_BLACK = "rgba(0, 0, 0, 0.6)";
+export const LIGHT_PRIMARY_BLUE = "rgba(36, 171, 224, 0.08)";
+export const AZUREISH_WHITE = "#D7EBF4";
+export const ERROR_RED = "#CF1E1E";
+export const WARNING_YELLOW = "#F8E224";
+export const WARNING_ORANGE = "#F89624";
+export const COMET_GRAY = "#566F7D";
+export const BLACK_CORAL = "#5C5868";
+export const ROMAN_SILVER = "#8E8B98";
+export const AURO_METAL = "#667080";
+export const AURO_METAL_FADED = "#66708066";
+export const SKY_BLUE = "#5FB6DC";
+export const BLUE_MATISSE = "#1D6D8F";
+export const SAIL_BLUE = "rgba(36, 171, 224, 0.08)";
+export const LIGHT_GRAY = "#D3D3D3";
+export const DARK_GRAY = "#5C5B70";
+export const MILKY_WHITE = "#F0F0F0";
+export const VERY_LIGHT_GRAY = "#E5E5E5";
+export const LIGHT_BORDER = "rgba(0, 0, 0, 0.23)";
+export const WARNING_TEXT = "#9D5500";
+export const PLACEHOLDER_TEXT = "#C2C2C2";
+export const ASH_GRAY = "#B5B2BE";
+export const PENDING_ORANGE = "FF9900";
