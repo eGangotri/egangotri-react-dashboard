63be9a3a6bd961c74f0ee292bea6f527088662fa
eGangotri Digital Preservation Trust <97491215+chetanbiqmind@users.noreply.github.com>
2022-11-07 13:58:54 +0800

    aesthetic issues in Layour of Uploads fixed.

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 import 'assets/css/index.css';
 import Dashboard from 'pages/Dashboard';
 import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Dashboard />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = createRoot(document.getElementById('root') as Element);
+root.render(<Dashboard />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
