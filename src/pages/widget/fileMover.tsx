e8612dfc5f4bfb4d3b8f6b91dc45fd7f567c0f94
egangotri <egangotritrust@gmail.com>
2023-04-10 19:32:09 +0800

    file mover

---
+++ src/pages/widget/fileMover.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Box } from '@mui/material';
+import TransferList from 'pages/widget/trransferList';
+
+const FileMover: React.FC = () => {
+    return (
+        <Box>
+            <TransferList />
+        </Box>
+    );
+}
+
+export default FileMover;
