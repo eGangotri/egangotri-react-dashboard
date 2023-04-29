4637e5a7bf631c26120541f447c0423f5906914a
egangotri <egangotritrust@gmail.com>
2023-04-29 06:34:08 +0530

    removed code not needed now

--- src/pages/widget/fileMover.tsx
+++ src/pages/widget/fileMover.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Box } from "@mui/material";
 import TransferList from "pages/widget/trransferList";
-import { FilePicker } from "components/file-picker";
 
 const FileMover: React.FC = () => {
   return (
     <Box>
       <TransferList />
       <div className="App">
-      <FilePicker accept="*.*"uploadURL={"http://dlptest.com/http-post/"} />
+        {//https://retool.com/blog/building-a-file-picker-component-in-react/
+        //https://codesandbox.io/s/filepicker-new-forked-3mipw9?ref=retool.com&file=/src/logo.svg
+        }
       </div>
     </Box>
   );
