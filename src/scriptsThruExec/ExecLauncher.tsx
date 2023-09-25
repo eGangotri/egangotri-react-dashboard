1a86f39aab5833e550a6c9882e76121f00cb1e1e
eGangotri <egangotritrust@gmail.com>
2023-09-25 09:40:14 +0530

    ;

--- src/scriptsThruExec/ExecLauncher.tsx
+++ src/scriptsThruExec/ExecLauncher.tsx
@@ -5,34 +5,13 @@ import ExecComponent from './ExecComponent';
 
 
 const ExecLauncher: React.FC = () => {
-    const [profiles, setProfiles] = useState('');
-
-    const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setProfiles(event.target.value);
-    };
-
-
-    const launch = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>, code = 1) => {
-        switch (code) {
-            case 1:
-                console.log('e', profiles, code);
-                launchGradleMoveToFreeze(profiles);
-                break;
-            default:
-                console.log('e', e.currentTarget.value, code)
-        }
-    }
 
     return (
-        // <>
-        // <input type="text" id="au" placeholder= value={profiles} onChange={onChange}></input>&nbsp;
-        // <button onClick={(e) => launch(e,1)}>Launch Archive-Uploader</button>
-        // </>
         <div>
             <ExecComponent buttonText="Upload Pdfs for Profile" />
-            <ExecComponent buttonText="Reverse Move (Python)"/>
-            <ExecComponent buttonText="Move Folder Contents" placeholder='Move QA-Passed-to-Pipeline'/>
-            <ExecComponent buttonText="Use Bulk Rename Conventions" placeholder='Use Bulk Rename Conventions'/>
+            <ExecComponent buttonText="Reverse Move (Python)" />
+            <ExecComponent buttonText="Move Folder Contents" placeholder='Move QA-Passed-to-Pipeline' />
+            <ExecComponent buttonText="Use Bulk Rename Conventions" placeholder='Use Bulk Rename Conventions' />
         </div>
     );
 }
---
+++ src/service/launchGradle.tsx
@@ -0,0 +1,19 @@
+import { backendServer } from 'utils/constants';
+
+export async function launchGradleMoveToFreeze(profiles: string) {
+    const _url = `${backendServer}launchGradle/moveToFreeze?profiles=${profiles}`
+    console.log(`_url ${_url}`);
+    const res = await fetch(_url);
+    const jsonResp = res.json()
+    console.log(`res ${JSON.stringify(jsonResp)}`);
+    return jsonResp;
+}
+
+export async function launchUploader(profiles: string) {
+    const _url = `${backendServer}launchGradle/launchUploader?profiles=${profiles}`
+    console.log(`_url ${_url}`);
+    const res = await fetch(_url);
+    const jsonResp = res.json()
+    console.log(`res ${JSON.stringify(jsonResp)}`);
+    return jsonResp;
+}
