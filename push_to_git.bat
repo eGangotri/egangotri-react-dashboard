0403d9d8cbc28c9131f2e05edacf8d9b11e17fb5
writetochetanpandey <writetochetanpandey@gmail.com>
2024-04-08 04:28:40 +0530

    Optimizations at 08-04-2024_4-28-40.79 ='

---
+++ push_to_git.bat
@@ -0,0 +1,11 @@
+set timestamp=%DATE:/=-%_%TIME::=-%
+set timestamp=%timestamp: =%
+set arg1=%1
+set arg1WithoutQuotes=%arg1:"='%
+set commit_msg="Optimizations at %timestamp% %arg1WithoutQuotes%"
+git status
+git add src/*
+git commit -m %commit_msg%
+git push origin master
+git status
+
