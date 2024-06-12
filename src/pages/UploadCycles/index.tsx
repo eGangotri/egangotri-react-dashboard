aaa1f507c210c6a4fb9d6df486757d2d0f91e279
Chandigarh Server <drnar@parthsharma>
2024-06-12 18:27:40 +0530

    Optimizations at 12-06-2024_18-27-40.78 ='

--- src/pages/UploadCycles/index.tsx
+++ src/pages/UploadCycles/index.tsx
@@ -376,9 +376,9 @@ const UploadCycles = () => {
                     {row.countIntended}
                     <Typography>{modePanel}</Typography>
                     {row?.archiveProfileAndCountIntended?.map((archiveProfileAndCount: UploadCycleArchiveProfile) => (
-                        <Box>
+                        <Box >
                             <Typography component="span">{archiveProfileAndCount.archiveProfile} </Typography>
-                            <Typography component="span">{archiveProfileAndCount.archiveProfilePath} </Typography>
+                            <Typography component="div">{archiveProfileAndCount.archiveProfilePath} </Typography>
                             <Typography component="span">{archiveProfileAndCount.count}</Typography>
                             <Typography component="div" sx={{ fontWeight: 600 }}>
                                 <Button
