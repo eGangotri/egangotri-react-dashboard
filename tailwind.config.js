1dbf092d4d3c637e0b14adc5af80e0f5f2ae608c
writetochetanpandey <writetochetanpandey@gmail.com>
2024-10-12 15:22:32 +0800

    Optimizations at Sat10-12-2024_15-22-32.15 ='

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        turquoise: {
+          50: '#E0F7F7',
+          100: '#B3EBEB',
+          200: '#80DEDE',
+          300: '#4DD1D1',
+          400: '#26C7C7',
+          500: '#00BDBD',
+          600: '#00A5A5',
+          700: '#008888',
+          800: '#006B6B',
+          900: '#004F4F',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
