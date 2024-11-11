5d8a5d6bf8407dca62be569e936569e06fa7afd9
eGangotri Digital Preservation Trust <egangotritrust@gmail.com>
2024-11-11 17:26:41 +0800

    Optimizations at Mon11-11-2024_17-26-39.79 ='

--- src/pages/UploadCycles/ColorCodedInformationPanel.tsx
+++ src/pages/UploadCycles/ColorCodedInformationPanel.tsx
@@ -7,7 +7,7 @@ export const ColorCodeInformationPanel = () => {
             <ul>
                 <li className="text-yellow-500">Yellow Row Highlight Color implies Intended Count mismatch</li>
                 <li className="text-red-800">Burgundy Red implies Failed Uploads</li>
-                <li className="text-brown-600">Brown implies All Non-Missed uploaded and verfied but check for Missed</li>
+                <li className="text-amber-600">Amber implies has Missed but Non-Missed uploaded and verfied</li>
                 <li className="text-green-600">Green implies All Intended Uploads uploaded and verfied</li>
                 <li>White implies All Uploaded but Actual Upload Verification not done.</li>
             </ul>
--- src/pages/UploadCycles/utils.ts
+++ src/pages/UploadCycles/utils.ts
@@ -1,14 +1,13 @@
-import { BURGUNDY_RED_TW, LIGHT_YELLOW_TW, PARTIAL_SUCCESS_BROWN_TW, SUCCESS_GREEN_TW } from "constants/colors"
+import { BURGUNDY_RED_TW, LIGHT_YELLOW_TW, PARTIAL_SUCCESS_AMBER_TW, SUCCESS_GREEN_TW } from "constants/colors"
 import { UploadCycleTableData } from "mirror/types"
 
 export const createBackgroundForRow = (row: UploadCycleTableData) => {
-    if ((row?.allUploadVerified === true) && (row?.countIntended !== row?.totalCount)) {
-        return PARTIAL_SUCCESS_BROWN_TW
+    if ((row?.allUploadVerified === false) && (row?.countIntended !== row?.totalCount)) {
+        return PARTIAL_SUCCESS_AMBER_TW
     }
     if (row?.countIntended !== row?.totalCount) {
         return LIGHT_YELLOW_TW
     }
-
     if (row?.allUploadVerified === true) {
         return SUCCESS_GREEN_TW
     }
