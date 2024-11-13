bdfc0496c83d6afcda99d8439ee635ea43bb8123
eGangotri Digital Preservation Trust <egangotritrust@gmail.com>
2024-11-13 11:19:32 +0800

    Optimizations at Wed11-13-2024_11-19-31.54 ='

--- src/pages/UploadCycles/ColorCodedInformationPanel.tsx
+++ src/pages/UploadCycles/ColorCodedInformationPanel.tsx
@@ -5,11 +5,11 @@ import { RED_TO_YELLOW } from 'constants/colors';
 export const ColorCodeInformationPanel = () => {
     return (
         <Typography variant="h6">
-            <ul>
+            <ul className="text-sm">
                 <li className="text-yellow-500">Yellow Row Highlight Color implies Missed Uploads</li>
-                <li className="red-yellow-gradient-color">Yellow-Red Gradient implies Failed Uploads ANd Missed Uploads</li>
+                <li className="red-yellow-gradient-color">Yellow-Red Gradient implies Failed Uploads & Missed Uploads</li>
                 <li className="text-red-800">Burgundy Red implies Failed Uploads But not Missed</li>
-                <li className="text-green-600">Green implies All Intended Uploads uploaded and verfied</li>
+                <li className="text-green-600">Green implies All Intended Uploads uploaded & verfied</li>
                 <li>White implies All Uploaded but Actual Upload Verification not done.</li>
             </ul>
         </Typography>
--- src/widgets/Spinner.tsx
+++ src/widgets/Spinner.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
-import './Spinner.css';
 import { Box } from '@mui/material';
 
 const Spinner = () => {
   return (
-    <Box component="span"  className="spinner-container">
-      <Box component="span" className="spinner"></Box>
+    <Box className="flex justify-center items-center">
+      <Box className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-gray-900">Loading</Box>
     </Box>
   );
 };
