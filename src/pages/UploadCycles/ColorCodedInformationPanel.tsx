edd397231fa65d4b554d468952b1bf0bfd2f510f
eGangotri Digital Preservation Trust <egangotritrust@gmail.com>
2024-11-14 14:59:20 +0800

    Optimizations at Thu11-14-2024_14-59-19.61 ='

--- src/pages/UploadCycles/ColorCodedInformationPanel.tsx
+++ src/pages/UploadCycles/ColorCodedInformationPanel.tsx
@@ -7,7 +7,7 @@ export const ColorCodeInformationPanel = () => {
         <Typography variant="h6">
             <ul className="text-sm">
                 <li className="text-yellow-500">Yellow Row Highlight Color implies Missed Uploads</li>
-                <li className="red-yellow-gradient-color">Yellow-Red Gradient implies Failed Uploads & Missed Uploads</li>
+                <li className="yellow-red-gradient-color">Yellow-Red Gradient implies Failed Uploads & Missed Uploads</li>
                 <li className="text-red-800">Burgundy Red implies Failed Uploads But not Missed</li>
                 <li className="text-green-600">Green implies All Intended Uploads uploaded & verfied</li>
                 <li>White implies All Uploaded but Actual Upload Verification not done.</li>
