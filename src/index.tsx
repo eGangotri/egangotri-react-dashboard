bed471a71e927f1fa1ab2a24ecac7ae15264ace3
writetochetanpandey <writetochetanpandey@gmail.com>
2025-11-16 00:19:48 +0530

    Optimizations at Sun11-16-2025_0-19-48.26 ='

--- src/index.tsx
+++ src/index.tsx
@@ -16,7 +16,7 @@ const root = createRoot(document.getElementById('root') as Element);
  * 
  */
 const googleClientID = import.meta.env.VITE_GOOGLE_CLIENT_ID || '';
-
+const x = React.createElement("div", null, "Hello World");
 root.render(
   <GoogleOAuthProvider clientId={googleClientID}>
     <BrowserRouter>
@@ -28,4 +28,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
+//reportWebVitals(console.log);
