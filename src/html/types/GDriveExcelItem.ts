a6c2808157bcf3e0c969a77e83049577109dbea9
eGangotri Digital Preservation Trust <egangotritrust@gmail.com>
2026-07-14 09:28:21 +0530

    Optimizations at Tue07-14-2026_9-28-20.86 ='

---
+++ src/html/types/GDriveExcelItem.ts
@@ -0,0 +1,14 @@
+export interface GDriveExcelItem {
+    "S.No": number;
+    "Title in Google Drive": string;
+    "Link to File Location": string;
+    "No. of Pages": number;
+    "Size with Units": string;
+    "Size in Bytes": string;
+    "Folder Name": string;
+    "Thumbnail": string;
+    "Created Time": string;
+}
+
+
+   
---
+++ src/html/types/HtmlDataType.ts
@@ -0,0 +1,21 @@
+export interface HtmlDataType {
+    t: string;
+    l: string;
+    p: string;
+    s: string;
+    sb: string;
+    f: string;
+    th: string;
+    c: string;
+}
+
+/**
+ *   "Title in Google Drive": "t"
+    "Link to File Location": "l"
+    "No. of Pages": "p"
+    "Size with Units": "s"
+    "Size in Bytes": "sb"
+    "Folder Name": "f"
+    "Thumbnail": "th"
+    "Created Time": "c"
+ */
