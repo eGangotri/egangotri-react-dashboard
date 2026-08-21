ac72b27b5324bd46cb297f91aaef6f262017a21b
eGangotri Digital Preservation Trust <egangotritrust@gmail.com>
2026-08-21 11:17:59 +0530

    Optimizations at Fri08-21-2026_11-17-57.37 ='

--- src/html/constants.ts
+++ src/html/constants.ts
@@ -6,7 +6,8 @@ export const INPUT_PATH = path.join(process.cwd(), `./src/html/input/`);
 export const MASTER_JSON = path.join(INPUT_PATH, 'master-data.json');
 export const BACKUP_DIR = path.join(INPUT_PATH, 'backup');
 
-const injectAbleExcel = "latest-32.xlsx"
+//pnpm run excelToHTML
+const injectAbleExcel = "latest-39.xlsx" //37-40 rem. start next from 51
 export const LATEST_INJECTABLE_EXCEL =  path.join(INPUT_PATH, injectAbleExcel);
 
 export const TEMPLATE_PATH = path.join(INPUT_PATH, 'GDrive_Explorer_Ultra-tmplt.html');
