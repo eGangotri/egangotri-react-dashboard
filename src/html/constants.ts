64d31062d53f3274c5110622be589116e1fd8291
eGangotri Digital Preservation Trust <egangotritrust@gmail.com>
2026-08-30 11:30:47 +0530

    Optimizations at Sun08-30-2026_11-30-46.20 ='

--- src/html/constants.ts
+++ src/html/constants.ts
@@ -3,15 +3,16 @@ import { HtmlDataType } from "./types/HtmlDataType";
 import * as path from 'path';
 
 export const INPUT_PATH = path.join(process.cwd(), `./src/html/input/`);
-export const MASTER_JSON = path.join(INPUT_PATH, 'master-data.json');
+export const MASTER_PATH = path.join(process.cwd(), `./src/html/master/`);
+export const MASTER_JSON = path.join(MASTER_PATH, 'master-data.json');
 export const BACKUP_DIR = path.join(INPUT_PATH, 'backup');
 
 //pnpm run excelToHTML
 const injectAbleExcel = "latest-78.xlsx" //start with  80.
 export const LATEST_INJECTABLE_EXCEL =  path.join(INPUT_PATH, injectAbleExcel);
 
-export const TEMPLATE_PATH = path.join(INPUT_PATH, 'GDrive_Explorer_Ultra-tmplt.html');
-export const FINAL_HTML_PATH = path.join(INPUT_PATH, 'GDrive_Explorer_Ultra.html');
+export const TEMPLATE_PATH = path.join(MASTER_PATH, 'GDrive_Explorer_Ultra-tmplt.html');
+export const FINAL_HTML_PATH = path.join(MASTER_PATH, 'GDrive_Explorer_Ultra.html');
 
 export const REQUIRED_GDRIVE_KEYS: (keyof GDriveExcelItem)[] = [
     'S.No',
